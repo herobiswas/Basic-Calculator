2e254936a4a8b2bd3864c786d371b434bff443c9
Hero Biswas <130970602+herobiswas@users.noreply.github.com>
2024-02-12 12:49:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('.calculator button');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (button.id === 'clear') {
+                display.value = '';
+            }
+            else if (button.id === 'delete') {
+                display.value = display.value.substr(0 , display.value.length -1);
+            }
+            else if (button.id === 'equals') {
+                try {
+                    display.value = eval(display.value);
+                } catch (error) {
+                    display.value = 'Error';
+                }
+            }
+            else {
+                display.value += button.textContent;
+            }
+        });
+    });
+});
